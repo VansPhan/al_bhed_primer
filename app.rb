63c0b10459412351955d8b4b957a2a0fc2102cab
Van Phan <VanPhan514@gmail.com>
2016-08-02 09:44:37 -0400

    Initial commit

---
+++ app.rb
@@ -0,0 +1,15 @@
+def cipher str
+	key = "ypltavkrezgmshubxncdijfqow".split("")
+	val = "abcdefghijklmnopqrstuvwxyz".split("")
+	hsh = Hash[key.zip(val)]
+	arr = str.downcase.split("")
+	output = ""
+	arr.each do |i|
+		if i == " "
+			output << " "
+		else
+			output << hsh[i]
+		end
+	end
+	puts output
+end
