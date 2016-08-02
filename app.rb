821ed298cb0f3713a1d973f04670c8a86144dbd6
Van Phan <VanPhan514@gmail.com>
2016-08-02 09:56:03 -0400

    Added decode method

--- app.rb
+++ app.rb
@@ -13,3 +13,19 @@ def cipher str
 	end
 	puts output
 end
+
+def decode str
+	key = "ypltavkrezgmshubxncdijfqow".split("")
+	val = "abcdefghijklmnopqrstuvwxyz".split("")
+	hsh = Hash[val.zip(key)]
+	arr = str.downcase.split("")
+	output = ""
+	arr.each do |i|
+		if i == " "
+			output << " "
+		else
+			output << hsh[i]
+		end
+	end
+	puts output
+end
